87d62cc3c7b6f554818ce01eec2804821f304b38
AnyanyaRog <anyaroganova2002@yandex.ru>
2023-04-15 22:55:37 +0700

    matrix

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains ) {
+  let count = {};
+ for (let i=0;i<domains.length;i++) {
+  let site = domains[i].split('.').reverse()
+  let current = '';
+  for (const char of site) {
+    current += `.${char}`;
+    if (current in count) {
+      count[current]++;
+    }  else {
+      count[current] = 1;
+    }
+
+   
+  }
+ }
+ return count;
 }
 
 module.exports = {
